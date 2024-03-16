fdb60d9da312121aa872755a1648680286952aa8
cass <casswoo@gmail.com>
2024-03-16 11:42:19 +0900

    wip

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Team
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+    #list_display = ('name', 'description', 'created_date', 'contact_email', 'is_active')
+    #list_filter = ['created_date']
+    #search_fields = ['name', 'description']
