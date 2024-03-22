8f7a36fa9b0f5bcf0530d0f028d991af28a5570c
cass <casswoo@gmail.com>
2024-03-23 08:07:41 +0900

    wip

--- hello/urls.py
+++ hello/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from hello import views
 urlpatterns = [
     path("", views.index, name="index"),
+    path("hello", views.hello, name="hello"),
+    path("test/<int:id>/", views.test, name="test"),
 ]
